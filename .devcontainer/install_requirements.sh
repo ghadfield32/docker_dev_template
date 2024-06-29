2df7a521c40dfbb2b2861f74b4707c81c56f543c
Geoffrey <ghadfield32@gmail.com>
2024-06-29 07:01:04 -0400

    updating to include the template repo for docker/conda

--- .devcontainer/install_requirements.sh
+++ .devcontainer/install_requirements.sh
@@ -3,8 +3,12 @@ ENV_NAME=$1
 PYTHON_VER=$2
 CPU=$(uname -m)
 
-
-# Install miniconda
+# Install Miniconda
+# apt-get update updates the package list.
+# wget downloads the Miniconda installer script.
+# /bin/bash executes the installer script.
+# Options:
+#    Change the URL to download a different version of Miniconda if needed.
 apt update && apt-get install -y --no-install-recommends \
     software-properties-common \
     && add-apt-repository -y ppa:deadsnakes/ppa \
@@ -17,6 +21,7 @@ wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-${CPU}.
     && conda install conda-build
 
 # Set environment
+# Create a new Conda environment and activate it.
 . /root/.bashrc \
     && conda create -y --name $ENV_NAME python=$PYTHON_VER 
 
@@ -25,5 +30,6 @@ echo "conda activate $ENV_NAME" >> ~/.bashrc
 
 conda activate $ENV_NAME
 
-# # Install the Python packages
+# Install the Python packages
+# pip3 install installs the packages listed in requirements.txt.
 pip3 install -r /requirements/requirements.txt
